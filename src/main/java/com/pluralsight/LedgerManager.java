d950506b5f286031e885ca92e5ba3b4ae08161c5
Dollant <ariawilliams2425@gmail.com>
2026-04-30 18:38:46 -0400

    Added load method to read transactions.csv into memory on app startup.

--- src/main/java/com/pluralsight/LedgerManager.java
+++ src/main/java/com/pluralsight/LedgerManager.java
@@ -12,5 +12,9 @@ public class LedgerManager {
 
     private List<Transactions> transactions = new ArrayList<>();
 
+    public void load() throws IOException {
+        Path path = Path.of(CSV_FILE);
+    }
+
 
 }
