099b93428cc78b72695f77f74c65146f0503f8b6
Dollant <ariawilliams2425@gmail.com>
2026-04-30 18:59:32 -0400

    Last day of previous month = first day of current month minus one day.

--- src/main/java/com/pluralsight/LedgerManager.java
+++ src/main/java/com/pluralsight/LedgerManager.java
@@ -61,6 +61,8 @@ public class LedgerManager {
     public List<Transactions> getPreviousMonth() {
         LocalDate now = LocalDate.now();
         LocalDate firstOfPrev = now.minusMonths(1).withDayOfMonth(1);
+        LocalDate lastOfPrev = now.withDayOfMonth(1).minusDays(1);
+        return filterByDateRange(firstOfPrev, lastOfPrev);
     }
 
 }
