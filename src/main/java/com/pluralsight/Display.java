e390fd0542f874bbfe19caa2daed5dd7d40a7554
Dollant <ariawilliams2425@gmail.com>
2026-04-30 19:32:58 -0400

    Added showHomeScreen to render the main hub menu with all four options.

--- src/main/java/com/pluralsight/Display.java
+++ src/main/java/com/pluralsight/Display.java
@@ -21,4 +21,20 @@ public class Display {
         System.out.println();
     }
 
+    public static void showHomeScreen() {
+        showSplash();
+        System.out.println(DOUBLE_DIV);
+        System.out.println("  ♡ MAIN HUB ♡");
+        System.out.println(DOUBLE_DIV);
+        System.out.println();
+        System.out.println("  [D]  Add Deposit        -- Log incoming gold");
+        System.out.println("  [P]  Make Payment       -- Record a purchase/debit");
+        System.out.println("  [L]  Open Ledger        -- View your transaction log");
+        System.out.println("  [X]  Exit               -- Save & quit");
+        System.out.println();
+        System.out.println(DIVIDER);
+        System.out.print("  > Enter command: ");
+    }
+
+
 }
